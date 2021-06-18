46cd3ec77b657d1a417f8871c8c259b6dd6d0a05
Bhanjo <hanjo1515@naver.com>
2021-06-18 15:36:22 +0900

    0616

--- practice02_wordStrain/client.jsx
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-// 컴포넌트 불러오기
-const wordRelay = require('./wordRelay');
-
-ReactDom.render(<wordRelay/>, document.querySelector('#root'));
