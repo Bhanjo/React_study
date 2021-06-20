33ae2f5d6ece1ff25c0f86fd7cf0478c28db2483
Bhanjo <hanjo1515@naver.com>
2021-06-20 16:20:06 +0900

    0620_리액트 컴포넌트 연습

--- leture2/start-react-app/src/App.js
+++ leture2/start-react-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'; //클래스 방식 적용
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+// 함수방식과 클래스방식 중 클래스방식 채택
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
+    );
+  }
+}
+class Navigation extends Component {
+  render() {
+    return (
+      <nav><ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+      </ul></nav>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* 컴포넌트 불러오기 */}
+        <Subject />
+        <Navigation />
+
+        Hello, React!!
+      </div>
+    );
+  }
 }
 
 export default App;
