5f55378964efafa5c4ca973c9668c8ab9c80fc71
Bhanjo <hanjo1515@naver.com>
2021-07-04 13:15:02 +0900

    0704_state공부

--- book_react/src/App.js
+++ book_react/src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import MyComponent from './MyComponent';
+// import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  const name = "리액트";
-  const style = {
-    backgroundColor: "black",
-    color: "white",
-  };
-  return (
-    <div style={style}>
-      {name === "리액트" ? <h1>{name}hello React!</h1> : <h1>not react..</h1>}
-      {/* <MyComponent name="React" /> */}
-      <MyComponent name={'리액트'} favNum={8}>리액트</MyComponent>
-    </div>
-  );
-}
+  return <Counter />
+};
 
 export default App;
