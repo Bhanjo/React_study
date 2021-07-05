80518823deb47591a5226755d78f9de6c857e9d8
Bhanjo <hanjo1515@naver.com>
2021-07-05 16:29:58 +0900

    ref in component

--- book_react/src/App.js
+++ book_react/src/App.js
@@ -1,14 +1,20 @@
+import { Component } from "react";
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from './Say';
 // import EventPractice from "./EventPractice";
-import { Component } from "react";
-import ValidationSasmple from "./ValidationSample";
+// import ValidationSasmple from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
     return (
-      <ValidationSasmple />
+      <div>
+        {/* 컴포넌트에 ref를 사용 */}
+        <ScrollBox ref={(ref) => this.scrollBox=ref} />
+        {/* 컴포넌트 내부의 매서드 사용 */}
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
     );
   }
 }
