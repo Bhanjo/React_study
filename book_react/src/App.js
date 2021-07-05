cd9568e517cd87602e45b98e4583e4e318095c14
Bhanjo <hanjo1515@naver.com>
2021-07-05 15:44:08 +0900

    input ref

--- book_react/src/App.js
+++ book_react/src/App.js
@@ -1,10 +1,16 @@
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from './Say';
-import EventPractice from "./EventPractice";
+// import EventPractice from "./EventPractice";
+import { Component } from "react";
+import ValidationSasmple from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice />
-};
+class App extends Component {
+  render() {
+    return (
+      <ValidationSasmple />
+    );
+  }
+}
 
 export default App;
