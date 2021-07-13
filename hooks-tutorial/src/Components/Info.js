c652b79fc50448cf3ea6c98522cabbc89b45f753
Bhanjo <hanjo1515@naver.com>
2021-07-13 22:51:19 +0900

    hooks-custom hooks

--- hooks-tutorial/src/Components/Info.js
+++ hooks-tutorial/src/Components/Info.js
@@ -1,22 +1,20 @@
 import React, { useReducer} from 'react';
+import useInputs from './useInputs'; // 커스텀 hooks
 
 // 리듀서 사용
-function reducer(state, action) {
-    return {
-        ...state,
-        [action.name]: action.value
-    };
-}
+// function reducer(state, action) {
+//     return {
+//         ...state,
+//         [action.name]: action.value
+//     };
+// }
 
 const Info = () => {
-    const [state, dispatch] = useReducer(reducer, {
+    const [state, onChange] = useInputs({
         name: '',
         nickname: ''
     });
     const { name, nickname } = state;
-    const onChange = e => {
-        dispatch(e.target);
-    };
 
     return (
         <div>
