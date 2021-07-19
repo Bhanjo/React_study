e2a1773679f40d4119af2123041d59fc69f2f7d6
Bhanjo <hanjo1515@naver.com>
2021-07-19 16:34:11 +0900

    추가: 리액트 라우터 초기 파일 생성

--- book_react/src/index.js
+++ book_react/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // 리액트 라우터 import
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
