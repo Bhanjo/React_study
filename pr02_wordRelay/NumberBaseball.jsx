f4d0ffd7226cf085a2581d45ee3311673d2aaba9
Bhanjo <hanjo1515@naver.com>
2021-07-19 16:23:43 +0900

    init

--- pr02_wordRelay/NumberBaseball.jsx
+++
@@ -1,9 +0,0 @@
-// import 방식
-import React, {Component} from 'react';
-
-class NumberBaseball extends Component {
-
-}
-
-export const hello = 'hello' //import {hello}
-export default NumberBaseball; //import NumberBaseball
