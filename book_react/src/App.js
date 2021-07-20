2e6ac32a067ba3803fc0a57a813a2ed3c0d27ec8
Bhanjo <hanjo1515@naver.com>
2021-07-20 11:22:11 +0900

    서브라우터 예제

--- book_react/src/App.js
+++ book_react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return(
@@ -10,13 +10,12 @@ const App = () => {
       <ul>
         <li><Link to="/">홈</Link></li>
         <li><Link to="/about">소개</Link></li>
-        <li><Link to="/profile/velopert">velopert 소개</Link></li>
-        <li><Link to="/profile/gildong">홍길동 소개</Link></li>
+        <li><Link to="/profiles">프로필</Link></li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
