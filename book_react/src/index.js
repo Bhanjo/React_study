484cad963cde941c471888f863edc85a73c0fdb5
Bhanjo <hanjo1515@naver.com>
2021-08-23 16:56:56 +0900

    추가: redux store 추가

--- book_react/src/index.js
+++ book_react/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux'; // 리덕스 store
+import { Provider } from 'react-redux';
+import rootReducer from './modules'
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root')
 );
