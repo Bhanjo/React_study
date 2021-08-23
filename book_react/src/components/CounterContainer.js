d9aa4760a1c6067a032b1c1c66c2eadf48f2c1ef
Bhanjo <hanjo1515@naver.com>
2021-08-23 17:35:14 +0900

    리팩토링: 카운터 컴포넌트의 mapDispatch toProps파라미터를 함수형에서 액션 생성 함수로 이루어진 객체 형태로 변경

--- book_react/src/components/CounterContainer.js
+++ book_react/src/components/CounterContainer.js
@@ -1,23 +1,17 @@
 import { connect } from "react-redux";
+import { decrease, increase } from "../modules/counter";
 import Counter from "./Counter";
 
 const CounterContainer = ({ number, increase, decrease }) => {
     return (<Counter number={number} onIncrease={increase} onDecrease={decrease} />);
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        console.log('증가');
-    },
-    decrease: () => {
-        console.log('감소');
-    },
-});
-
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    },
 )(CounterContainer);
