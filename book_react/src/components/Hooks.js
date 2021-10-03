c9086eb8c3d9c436a44090967bca6a5d3679ec8b
Bhanjo <hanjo1515@naver.com>
2021-10-03 20:29:07 +0900

    추가: 리듀서 함수 예제

--- book_react/src/components/Hooks.js
+++ book_react/src/components/Hooks.js
@@ -1,24 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useReducer } from 'react'
+
+const reducer = (state, action) => {
+    switch (action.type) { // action type에 따라 다른 값 변경
+        case 'INCREMENT':
+            return { number: state.number + 1};
+        case 'DECREMENT':
+            return { number: state.number - 1};
+        default:
+            return state;
+    }
+}
 
 const Hooks = () => {
-    const [name, setName] = useState('')
-    useEffect(() => {
-        console.log('useEffect를 실행합니다' + name);
-        return () => { // 뒷정리 함수 실행
-            console.log('뒷정리 함수가 실행됨'+ name);
-        };
-    },[name]);
-    const onChange = e => {
-        setName(e.target.value)
-    }
+    // const [name, setName] = useState('')
+    // useEffect(() => {
+    //     console.log('useEffect를 실행합니다' + name);
+    //     return () => { // 뒷정리 함수 실행
+    //         console.log('뒷정리 함수가 실행됨'+ name);
+    //     };
+    // },[name]);
+    // const onChange = e => {
+    //     setName(e.target.value)
+    // }
+
+    const [state, dispatch] = useReducer(reducer, { number: 0 })
+
     return(
         <>
             <h1>Hooks 사용 연습</h1>
-            <input
+            {/* <input
                 name='name'
                 onChange={onChange}
             />
-            <h1>입력: {name}</h1>
+            <h1>입력: {name}</h1> */}
+            <p>현재 값은 {state.number}</p>
+            <button onClick={() => dispatch({type: 'INCREMENT'})}>+ 1</button>
+            <button onClick={() => dispatch({type: 'DECREMENT'})}>- 1</button>
         </>
     );
 }
