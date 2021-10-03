243b8513666c96a3b947d506906973082116e32e
Bhanjo <hanjo1515@naver.com>
2021-10-03 20:57:52 +0900

    추가: useCallback 예제 추가

--- book_react/src/components/Hooks.js
+++ book_react/src/components/Hooks.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useCallback } from 'react'
 
 // 평균값 구하는 연산
 const getAverage = numbers => {
@@ -12,15 +12,16 @@ const Hooks = () => {
     const [list, setList] = useState([])
     const [number, setNumber] = useState(''); // 주의) 문자열로 받는다
 
-    const onChange = e => {
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    }
-    const onInsert = () => {
+    },[]) // 빈 배열 => 최초 렌더링 시 함수 생성
+
+    const onInsert = useCallback(e => {
         // 이전 리스트에 number를 추가한다(이때, number는 문자열이니 int로 형변환을 시킨다)
         const nextList = list.concat(parseInt(number))
         setList(nextList)
         setNumber('');
-    }
+    }, [number, list]) // number와 list가 변경되었을 때 함수 생성
 
     // useMemo 선언
     // list 값이 다르면 getAverage를 실행한다
